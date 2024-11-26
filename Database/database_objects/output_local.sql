b03d2f0a20fa443c0a3abb544be81c9ba347773f
Arman <130306534+SSJGodGogeta@users.noreply.github.com>
2024-11-26 18:01:09 +0100

    Added more dummy values.

--- Database/database_objects/output_local.sql
+++ Database/database_objects/output_local.sql
@@ -1,8 +1,8 @@
 
 UPDATE SUMMARY
 Run:                          1
-Previously run:               2
+Previously run:               6
 Filtered out:                 0
 -------------------------------
-Total change sets:            3
+Total change sets:            7
 
