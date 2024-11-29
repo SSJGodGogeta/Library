7ac648af8187f885c9a44e6f2c9b9df348b878d8
Arman <130306534+SSJGodGogeta@users.noreply.github.com>
2024-11-29 15:43:35 +0100

    naming convention... adding navbar dynamically through typescript. just import the navigation.js in future

---
+++ Webpage/navigation.ts
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded', () => {
+    loadNavbar();
+});
+
+const elements = {
+    homeLink: {
+        id: "homeLink",
+        name: "Home",
+        href: "index.html"
+    },
+    booksLink: {
+        id: "booksLink",
+        name: "Books",
+        href: "book.html"
+    },
+    newReleaseLink: {
+        id: "newReleaseLink",
+        name: "New Releases",
+        href: "newRelease.html"
+    },
+    myBooksLink: {
+        id: "myBooksLink",
+        name: "My Books",
+        href: "myBook.html"
+    },
+    myAccountLink: {
+        id: "myAccountLink",
+        name: "My Account",
+        href: "myAccount.html"
+    }
+}
+
+function loadNavbar(): void {
+    let navbarHTML = `
+        <img alt="Background" class="background" src="Images/Background.jpg">
+        <nav>`;
+
+    Object.values(elements).forEach(link => {
+        navbarHTML += `
+            <a href="${link.href}" id="${link.id}">${link.name}</a>
+        `;
+    });
+
+    // Add the search input at the end of the navbar
+    navbarHTML += `
+        <label>
+            <input placeholder="Search.." type="text">
+        </label>
+    </nav>`;
+
+    const navbarContainer = document.getElementById('navbar');
+    if (navbarContainer) {
+        navbarContainer.innerHTML = navbarHTML;
+    } else {
+        console.error("Navbar container with ID 'navbar' not found.");
+    }
+}
