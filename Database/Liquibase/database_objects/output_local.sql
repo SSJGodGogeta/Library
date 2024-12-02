00e94cd87440efab8c8f920708074fdb8d4d3087
Arman <130306534+SSJGodGogeta@users.noreply.github.com>
2024-12-02 13:06:44 +0100

    added output_local_sql to gitignore

--- Database/Liquibase/database_objects/output_local.sql
+++
@@ -1,8 +0,0 @@
-
-UPDATE SUMMARY
-Run:                         15
-Previously run:               0
-Filtered out:                 0
--------------------------------
-Total change sets:           15
-
